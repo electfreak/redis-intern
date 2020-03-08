880c958439e96da12ce819fc109023da43dfed38
Arseny Yusufov <senya.yusufov@yandex.ru>
2020-03-08 21:12:33 +0300

    backup

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import data from'./input.js';
+import data from './input.js';
 
 const stateModule = {
   state: {
@@ -38,7 +38,7 @@ const stateModule = {
 };
 
 const feodalContainer = document.querySelector('.main__feodal'),
-      vassals = document.querySelector('.main__vassals');
+  vassals = document.querySelector('.main__vassals');
 
 
 function render(state) {
@@ -58,43 +58,93 @@ function render(state) {
   // FEODAL'S RENDER
 
   const feodalObj = data.find(person => person.id === state.feodalId);
-  const feodalElem = createPerson(feodalObj);
+  const feodalElem = createPersonFeodal(feodalObj);
   feodalElem.classList.add('person_feodal');
   feodalElem.append(...createArrows(feodalObj));
-
 
   feodalContainer.append(feodalElem);
 
   // VASSAL'S RENDER 
-  
+
   const vassalsArr = data.filter(person => person.parent === state.feodalId),
-  vassalsElemArr = [];
+    vassalsElemArr = [];
 
   vassalsArr.forEach(person => {
-    let personElem = createPerson(person);
+    let personElem = createPersonVassal(person);
     personElem.classList.add('person_vassal');
-    
+
     vassalsElemArr.push(personElem);
   });
 
   vassalsElemArr.forEach(person => vassals.append((person)));
 }
 
-function createPerson({name, image, id}) {
+function createPerson({
+  name,
+  image,
+  id,
+  post = false
+}) {
   let person = document.createElement('div');
   person.classList.add('person');
-  
-  person.innerHTML = `<img class='person__img' src='img/${image}' alt='${name}'></img> <div class='person__title'>${name}</div>`;
 
-  person.addEventListener('click', () => {
-    stateModule.commit('openFeodal', id);
+  person.innerHTML =
+    `<div class='person__img-container'>
+      <img class='person__img' src='img/${image}' alt='${name}'></img>
+    </div> 
+
+  <h2 class='person__title'>${name}</h2>`;
+
+  // Count vassal's
+  let countVassals = 0;
+  data.forEach(item => {
+    if (item.parent === id) ++countVassals;
   })
+
+  if (countVassals) {
+    const countVassalsElem = document.createElement('div');
+    countVassalsElem.classList.add('person__count-vassals-container');
+    countVassalsElem.innerHTML = `<div class='person__count-vassals'>${countVassals}</div>`;
+
+    person.querySelector('.person__img-container').append(countVassalsElem);
+  }
+
+  if (post) {
+    let postElem = document.createElement('h2');
+    postElem.innerText = post;
+    postElem.classList.add('person__post');
+    person.append(postElem);
+  }
 
   return person;
 
 }
 
-function createArrows({id, parent}) {
+function createPersonFeodal(personObj) {
+  const feodalElem = createPerson(personObj);
+  return feodalElem;
+}
+
+function createPersonVassal(personObj) {
+  const vassalElem = createPerson(personObj);
+  const countVassals = vassalElem.querySelector('.person__count-vassals'),
+        {id} = personObj;
+  if (countVassals) {
+    vassalElem.style.cursor = 'pointer';
+
+    vassalElem.addEventListener('click', () => {
+      stateModule.dispatch('openFeodal', id);
+    });
+
+  }
+
+  return vassalElem;
+}
+
+function createArrows({
+  id,
+  parent
+}) {
   const back = document.createElement('div');
   back.classList.add('arrow-button', 'arrow-button_back');
 
@@ -102,9 +152,9 @@ function createArrows({id, parent}) {
   forward.className = 'arrow-button arrow-button_forward';
 
   let vassalsSingleLevel = data.filter(person => person.parent === parent);
-  
+
   console.log(vassalsSingleLevel);
-  
+
   let index = vassalsSingleLevel.findIndex(item => item.id === id);
 
   let backId = (index === 0) ? vassalsSingleLevel[vassalsSingleLevel.length - 1].id : vassalsSingleLevel[index - 1].id;
@@ -112,12 +162,18 @@ function createArrows({id, parent}) {
 
   console.log(backId, forwardId);
 
-  back.addEventListener('click', () => stateModule.dispatch('openFeodal', backId));
-  forward.addEventListener('click', () => stateModule.dispatch('openFeodal', forwardId));
+  back.addEventListener('click', (event) => {
+    event.stopPropagation();
+    stateModule.dispatch('openFeodal', backId);
+  });
+  forward.addEventListener('click', (event) => {
+    event.stopPropagation();
+    stateModule.dispatch('openFeodal', forwardId);
+  });
 
   return [back, forward];
 }
 
 stateModule.subscribe(render);
 
-stateModule.commit('openFeodal', 4);
+stateModule.commit('openFeodal', 1);
